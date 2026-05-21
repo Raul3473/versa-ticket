69ab5fea62efcb7357d96a8b3e61a993478692ae
Raul3473 <Wongaguilar49@gmail.com>
2026-05-21 08:53:54 -0600

    brevo para enviar correo

--- versa-ticket-back/config/mailer.js
+++ versa-ticket-back/config/mailer.js
@@ -1,13 +1,17 @@
 // config/mailer.js
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
     logger: true,
     debug: true
 });
