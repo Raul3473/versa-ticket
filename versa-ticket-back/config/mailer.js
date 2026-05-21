3c7ddaf4ce9d7551c136a48428f0f9821a19836b
Raul3473 <Wongaguilar49@gmail.com>
2026-05-21 08:01:57 -0600

    Error con el correo

--- versa-ticket-back/config/mailer.js
+++ versa-ticket-back/config/mailer.js
@@ -5,11 +5,14 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    family: 4, // Fuerza IPv4
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 module.exports = transporter;
