588ee6e13ceca0c8974e128d79da2b9c0d56b7d4
Raul3473 <Wongaguilar49@gmail.com>
2026-05-21 08:15:43 -0600

    lo que esta leyendo Render

--- versa-ticket-back/config/mailer.js
+++ versa-ticket-back/config/mailer.js
@@ -1,21 +1,17 @@
 // config/mailer.js
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS existe:", !!process.env.EMAIL_PASS);
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
 });
 
 module.exports = transporter;
